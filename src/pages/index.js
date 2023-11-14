615f19ce6ff5436abf18483e97be4600acf29c6b
nour hellani <nour.hellani@creoshift.com>
2023-11-14 15:15:54 +0200

    fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 export default function Home({}) {
   return (
     <div className="h-screen">
-      <p className="bg-text w-1/3 mx-auto">
+      <p className="mx-auto bg-text  w-2/3">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. sed do eiusmod
         tempor magna
       </p>
