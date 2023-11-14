47c0557f64f738b7a6e5ebfb5f32fd57839517c9
nour hellani <nour.hellani@creoshift.com>
2023-11-14 14:19:06 +0200

    text

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
-
-export default function Home({ }) {
-  
+import React from "react";
+export default function Home({}) {
   return (
-    
-    <div>
-      Nextjs
+    <div className="h-screen">
+      <p className="bg-text w-1/3 mx-auto">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. sed do eiusmod
+        tempor magna
+      </p>
     </div>
-  )
+  );
 }
-
